ecfda07650e2cdf4f1a86955cb249ae14d08dc8b
rshaikhha <rshaikhha@gmail.com>
2017-09-06 20:35:12 +0430

    renamed acount to account

--- account/apps.py
+++ account/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class AcountConfig(AppConfig):
-    name = 'acount'
+class AccountConfig(AppConfig):
+    name = 'account'
--- sharet/urls.py
+++ sharet/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from acount import views as acount_views
+from account import views as account_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', acount_views.home, name='home'),
+    url(r'^$', account_views.home, name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),        
-    url(r'^signup/$', acount_views.signup, name='signup'),
-    url(r'^account_activation_sent/$', acount_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^signup/$', account_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', account_views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        acount_views.activate, name='activate'),
+        account_views.activate, name='activate'),
 ]
