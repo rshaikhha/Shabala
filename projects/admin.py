89c3f2e2ed285efc0c17aafe2fa3b1a25780e31f
Sohiyel <soheil.baghery@gmail.com>
2017-09-08 13:22:49 +0430

    add Project and Tender models

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Project,Tender
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_time'
+    search_fields = []
+    list_display = ['__unicode__', 'description', 'created_time', 'dead_line']
+    list_editable = []
+    list_filter = []
+    readonly_fields = []
+    class Meta:
+        model = Project
+
+class TenderAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_time'
+    search_fields = []
+    list_display = ['__unicode__', 'price', 'description', 'created_time', 'needed_time']
+    list_editable = []
+    list_filter = []
+    readonly_fields = []
+    class Meta:
+        model = Tender
+
+
+admin.site.register(Tender,TenderAdmin)
+admin.site.register(Project,ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True,auto_now=False)
+    dead_line = models.DateTimeField(auto_now_add=False,auto_now=False)
+    public = models.BooleanField(default=True)
+    #tender = models.ManyToManyField(Profile)
+
+    def __unicode__(self):
+        return self.title
+
+class Tender(models.Model):
+    tender = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    price = models.BigIntegerField()
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True,auto_now=False)
+    needed_time = models.DateTimeField(auto_now_add=False,auto_now=False)
+
+    def __unicode__(self):
+        return self.tender.username 
